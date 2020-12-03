f238eeff6337b05d9cce9d7624cdeb4d8a26190f
Avijit Karmaker <ajkacca@gmail.com>
2020-12-03 16:04:46 +0200

    modal styling and add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-const addButton = document.querySelector('.addbook');
-addButton.addEventListener("click", function() {
-    alert("as")
+const addBook = document.querySelector('.addbook');
+const modal= document.querySelector(".modal-d");
+const btncont=document.querySelector(".btncont");
+const btnclose=document.querySelector(".close");
+
+addBook.addEventListener("click", function() {
+modal.classList.add("show");
+btncont.classList.add("hide");
+})
+
+
+btnclose.addEventListener("click",function(){
+  modal.classList.remove("show");
+  btncont.classList.remove("hide");
 
 })
